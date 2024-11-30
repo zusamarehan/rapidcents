3115feb1f553e9311f01fb6a7816ee228f94f8da
Rehan Kanak <zrehan286@gmail.com>
2024-11-30 10:29:59 -0500

    setup: controllers

---
+++ app/Http/Controllers/RetrieveTransactionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RetrieveTransactionController extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CreateTransactionController;
+use App\Http\Controllers\RetrieveTransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/transactions/{id}', RetrieveTransactionController::class);
+Route::post('/transactions', CreateTransactionController::class);
