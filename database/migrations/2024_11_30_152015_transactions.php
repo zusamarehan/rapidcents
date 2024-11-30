e32a060c75807125f6c05dcbb8cf9cb55509f607
Rehan Kanak <zrehan286@gmail.com>
2024-11-30 10:24:37 -0500

    setup: migrations

---
+++ database/migrations/2024_11_30_152015_transactions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->uuid('transaction_id');
+            $table->string('card_number');
+            $table->integer('amount');
+            $table->string('currency');
+            $table->string('email');
+            $table->string('status');
+            $table->json('metadata');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('transactions');
+    }
+};
